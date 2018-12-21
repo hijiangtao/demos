95523eacf3fa78820362c2380589fe59f7b18ac0
hijiangtao <hijiangtao@gmail.com>
2018-12-21 14:43:23 +0800

    gh-pages first commit

---
+++ precache-manifest.fc0e5716e43aaa44b868daf8a1d5278a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7beb1ab22e489365b220",
+    "url": "/static/js/main.7beb1ab2.chunk.js"
+  },
+  {
+    "revision": "051298d28e6d3129d749",
+    "url": "/static/js/1.051298d2.chunk.js"
+  },
+  {
+    "revision": "7beb1ab22e489365b220",
+    "url": "/static/css/main.f4aeb471.chunk.css"
+  },
+  {
+    "revision": "5c9836a859a8ca8662836eb7fa780e4f",
+    "url": "/index.html"
+  }
+];
